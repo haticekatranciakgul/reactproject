91cb7ae53322edb8bb9e3155ee3f06bda7ebf15d
hatice <hatice@hotmail.com>
2022-01-08 14:49:29 +0300

    --comm

---
+++ donguler.js
@@ -0,0 +1,19 @@
+//console ekranında alt alta yazdırdık
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+
+//aynı işlemin kısayolu
+for (i = 1; i < 10; i++) {
+    console.log(1);
+}
+console.log("bitti")
+//10 dan küçük ve eşit olana kadar i yi 1 arttırarak git 
